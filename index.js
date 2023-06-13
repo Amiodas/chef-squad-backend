c5bc82b15f96f4fc5d1ffa4928716e52edd70586
Amiodas <amio.das.dip@gmail.com>
2023-06-13 10:31:53 +0100

    #data and images added

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require("express");
+var cors = require("cors");
 const app = express();
+
+app.use(cors());
+
 const port = process.env.PORT || 5000;
 
 const chefs = require("./data/chefs.json");
@@ -10,6 +14,13 @@ app.get("/", (req, res) => {
 
 app.get("/chefs", (req, res) => {
   res.send(chefs);
+});
+
+app.get("/chefs/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const selectedChef = chefs.find((chef) => chef.id === parseInt(id));
+  res.send(selectedChef);
 });
 
 app.listen(port, () => {
